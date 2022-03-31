f2edf92e04962ecdf03576c3b2b21b5b74b08bf6
Martyna <101583527+mrtvx@users.noreply.github.com>
2022-03-31 05:09:18 +0200

    Add files via upload

--- projekt_bielecka.R
+++ projekt_bielecka.R
@@ -1,3 +1,8 @@
+library(tidyverse)
+library(dplyr)
+
+### 
+
 menu = read.csv("MENU2.csv", encoding = "UTF-8")
 menu
 
@@ -8,151 +13,125 @@ menu_stare = read.csv("ceny1.csv", encoding = "UTF-8")
 menu_stare
 
 
-#PROGNOZY NA PRZYSZLOSC 
+#Prognoza wzrostu cen w przeciągu najbliższych lat (na podstawie zmiany średniej cen dań głównych na przestrzeni 6 miesięcy) 
 
-##############
 nowe_ceny = mean(menu$CENA[1:41])
 nowe_ceny
 wzrost = menu$CENA[1:41] - menu_stare$CENA
 wzrost
 zmiana = mean(wzrost)
 zmiana 
-wr=1.37
-sr= mean(menu_stare$CENA)
-sr = 10.96
-p =  c(sr, sr+1.37, sr+(2*1.37), sr+(3*1.37), sr+(4*1.37), sr+(5*1.37))
-y= (2021.5:2026) 
-y
+wzrost_roczny=1.37
+srednia_stare_ceny= mean(menu_stare$CENA)
+srednia_stare_ceny = 10.96
+prognoza =  c(srednia_stare_ceny, srednia_stare_ceny+1.37, srednia_stare_ceny+(2*1.37), srednia_stare_ceny+(3*1.37), srednia_stare_ceny+(4*1.37), srednia_stare_ceny+(5*1.37))
+lata= (2021.5:2026) 
+lata
 
-menu3 = data.frame(cena = c(sr, sr+1.37, sr+(2*1.37), sr+(3*1.37), sr+(4*1.37), sr+(5*1.37)),
+wzrost_cen = data.frame(cena = c(sr, sr+1.37, sr+(2*1.37), sr+(3*1.37), sr+(4*1.37), sr+(5*1.37)),
                    rok = 2021:2026)
-menu3
 
 
 
-ggplot(menu3, aes(x=cena,
+
+ggplot(wzrost_cen, aes(x=cena,
                  y=rok)) + stat_smooth()  + geom_point()
 
-length(x) 
-length(y)
 
-plot(x,y[1:length(x)], type = "l", main = "Prognoza wzrostu cen w bufecie WNGiG", col = "red",
-      xlab = "cena w złotówkach", ylab = "lata")
+##
+#funkcje pozwalające na wyodrębnienie ceny najtańszego i najgroższego elementu z danej kategorii 
+ 
+najtańsze = function(kategoria){
+  if (kategoria == "N"){
+    min(menu$CENA[menu$ID == "N"])}
+  else if (kategoria == "O"){
+    min(menu$CENA[menu$ID == "O"])}
+  else if (kategoria == "P"){
+    min(menu$CENA[menu$ID == "P"])}
+  else if (kategoria == "PR"){
+    min(menu$CENA[menu$ID == "PR"])}
+  else if (kategoria == "DS"){
+    min(menu$CENA[menu$ID == "DS"])}
+  else if (kategoria == "D"){
+    min(menu$CENA[menu$ID == "D"])}
+  else if (kategoria == "S"){
+    min(menu$CENA[menu$ID == "S"])}
+  
+}
 
-library(tidyverse)
+najtańsze("N")
 
+###
 
-
-
-
-
-##################
-
-
-
-library(dplyr)
-
-
-#NAJTAŃSZE_I_NAJDROŻSZE 
-
-Deser = menu %>%
-  filter(ID == "DS") %>%
-  select(NAZWA, CENA, CZY_VEGE)
-Deser
-
-Napoje = menu %>% 
-  filter(ID=="N") %>%
-  select(NAZWA,CENA, CZY_VEGE)
-Napoje 
-
-Dodatki = menu %>%
-  filter(ID=="D") %>%
-  select(NAZWA,CENA, CZY_VEGE)
-Dodatki 
-
-Dania = menu %>% 
-  filter (ID == "O") %>%
-  select(NAZWA,CENA, CZY_VEGE)
-Dania
-
-Kanapki = menu %>%
-  filter(ID =="K") %>% 
-  select(NAZWA,CENA, CZY_VEGE)
-
-Pizza = menu %>%
-  filter(ID == "P") %>%
-  select(NAZWA,CENA, CZY_VEGE)
-Pizza 
-
-sniadania = menu %>%
-  filter(ID == "S") %>% 
-  select (NAZWA,CENA, CZY_VEGE)
-sniadania 
-
-
-plot()
-
-max  = max(sniadania$NAZWA[sniadania$CENA])
-
-#SORTOWANIE POZYCJI WEGE 
-
-##################
-
-Vege_menu = menu %>%
-  filter(CZY_VEGE == "V") %>%
-  select(NAZWA, CENA)
-Vege_menu 
-
-Deser_V = Deser%>%
-  filter(CZY_VEGE == "V") %>%
-  select(NAZWA,CENA)
-Deser_V
-
-Dodatki_V = Dodatki %>%
-  filter(CZY_VEGE == "V") %>%
-  select(NAZWA,CENA)
-Dodatki_V 
-
-Dania_V = Dania %>% 
-  filter (CZY_VEGE == "V") %>%
-  select(NAZWA,CENA)
-Dania_V
-
-Kanapki_V = Kanapki %>%
-  filter(CZY_VEGE == "V") %>% 
-  select(NAZWA,CENA)
-Kanapki_V
-
-Pizza_V = Pizza %>%
-  filter(CZY_VEGE == "V") %>%
-  select(NAZWA,CENA)
-Pizza_V 
-
-sniadania_V = sniadania %>%
-  filter(CZY_VEGE == "V") %>% 
-  select (NAZWA,CENA)
-sniadania_V
-
-
-
-#WYKRES PROCENT VEGE DAN 
-
-#############
-
-menu_2 = menu$NAZWA[menu$ID != "N"]
-menu_2
-
-menu
-nrow(menu)
-length(menu_2)
-
-procent_vege = (length(menu_2)/nrow(menu))*100
-procent_vege
+najdroższe = function(kategoria){
+  if (kategoria == "N"){
+  max(menu$CENA[menu$ID == "N"])}
+  else if (kategoria == "O"){
+  max(menu$CENA[menu$ID == "O"])}
+  else if (kategoria == "P"){
+  max(menu$CENA[menu$ID == "P"])}
+  else if (kateoria == "PR"){
+  max(menu$CENA[menu$ID == "PR"])}
+  else if (kategoria == "DS"){
+  max(menu$CENA[menu$ID == "DS"])}
+  else if (kategoria == "D"){
+  max(menu$CENA[menu$ID == "D"])}
+  else if (kategoria == "S"){
+  max(menu$CENA[menu$ID == "S"])}
+}
+  
+najdroższe("O")
 
 
 
 
-menu_2
+#funkcja pozwalająca na wyodrębnienie pozycji wegetariańskich 
+
+
+Vege = function(kategoria){
+    if (kategoria == "O"){
+    menu$NAZWA[menu$ID == "O" & menu$CZY_VEGE == "V"]}
+    else if (kategoria == "P"){
+    menu$NAZWA[menu$ID == "P" & menu$CZY_VEGE == "V"]}
+    else if (kategoria == "PR"){
+    menu$NAZWA[menu$ID == "PR" & menu$CZY_VEGE == "V"]}
+    else if (kategoria == "DS"){
+    menu$NAZWA[menu$ID == "DS"& menu$CZY_VEGE == "V"]}
+    else if (kategoria == "D"){
+    menu$NAZWA[ menu$ID == "D" &menu$CZY_VEGE == "V"]}
+    else if (kategoria == "S"){
+    menu$NAZWA[menu$ID == "S" & menu$CZY_VEGE == "V"] }
+    }
+  
+Vege("S")
+
+
+
+
+#Funkcja pozwalająca sprawdzić użytkownikowi na jakie produkty z danej kategorii go stać 
+
+na_co_wystarczy = function(kategoria, kwota){
+  
+  if (kategoria == "N"){
+    menu$NAZWA[menu$CENA <= kwota &  menu$ID == "N"]}
+  else if (kategoria == "O"){
+    menu$NAZWA[menu$CENA <= kwota &  menu$ID == "O"]}
+  else if (kategoria == "P"){
+    menu$NAZWA[menu$CENA <= kwota &  menu$ID == "P"]}
+  else if (kategoria == "PR"){
+    menu$NAZWA[menu$CENA <= kwota &  menu$ID == "PR"]}
+  else if (kategoria == "DS"){
+    menu$NAZWA[menu$CENA <= kwota &  menu$ID == "DS"]}
+  else if (kategoria == "D"){
+    menu$NAZWA[menu$CENA <= kwota &  menu$ID == "D"]}
+  else if (kategoria == "S"){
+    menu$NAZWA[menu$CENA <= kwota &  menu$ID == "S"]}
+  else if (!is.character(kwota)){
+    print("Masz do wyboru kategorie: 'O' -obiad, 'P' - Pizza, 'S' - śniadanie, 'PR' - przekąska słona, 'DR' - deser, 'D' - dodatki, 'N' - Napój.")
+  }
+  
+}
+na_co_wystarczy("D", 2)
 
 
 #ZESTAW 
@@ -163,12 +142,9 @@ zestaw = function(A,B,D){
   A == menu$ID
   B == menu$ID
   D == menu$ID
-  sample(c(menu$NAZWA[menu$A], menu$NAZWA[menu$B], menu$NAZWA[menu$D]), 1)
+  sample(c(sample(menu$NAZWA[menu$ID == A],1), sample(menu$NAZWA[menu$ID == B],1), sample(menu$NAZWA[menu$ID == D],1)))
   }
 zestaw("O","N","DS")
-
-zestaw = c((sample(menu$NAZWA[ menu$ID == "N"], 1)), sample(menu$NAZWA[ menu$ID == "DS"], 1), sample(menu$NAZWA[menu$ID == "O"],1))
-zestaw
 
 
 
@@ -177,13 +153,15 @@ zestaw
 ##funkcja służąca do wyszukiwania możliwej do zakupu pizzy na podstawie podanego składnika 
 
 
-jaka_pizza = function(składnik){
-  grep("składnik", menu$SKŁADNIKI , value = TRUE)
-  }
-jaka_pizza(ser)
-
-
 grep("ananas", menu$SKŁADNIKI , value = TRUE)
+
+
+jaka_pizza = function(skladnik){
+  grep("skladnik", menu$SKŁADNIKI , value = TRUE)
+  menu$NAZWA[menu$ID == "P"]
+}
+
+jaka_pizza(ananas)
    
 
 
