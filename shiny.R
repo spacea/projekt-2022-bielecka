7996d3b32818417a8e2a0750e325ab54e638d900
dagusia1113 <102621788+dagusia1113@users.noreply.github.com>
2022-04-07 01:48:59 +0200

    Update shiny.R

--- shiny.R
+++ shiny.R
@@ -1,5 +1,6 @@
 menu = read.csv("menu.csv", encoding = "UTF-8")
 
+library(tidyverse)
 library(shiny)
 library(shinyWidgets)
 
@@ -8,33 +9,60 @@ ui <- fluidPage(
   titlePanel(span("Wylosuj swój zestaw", style = "color:magenta")),
   
   sidebarPanel( 
-    materialSwitch(inputId = "wege",label = "Czy jesteś wegeterianinem? ", value = FALSE),
-    actionButton("button", "kliknij tutaj"),
+  
+    numericInput("kwota", "Zaznacz kwotę, w której ma się mieścić zestaw:", 20, min = 20, max = 50),
+    
+    materialSwitch(inputId = "wege",label = "Zaznacz jeśli jesteś wegeterianinem. ", value = FALSE),
+    
+    prettyRadioButtons(
+      inputId = "radio",
+      label = "Czy chcesz wyświetlić wykres, pokazujący procentowy wzrost średniej ceny dania głównego? ( !UWAGA! Jeśli zaznaczysz 'TAK', nie będziesz już mógł cofnąć tej decyzji.)",
+      icon = icon("check"),
+      choices = c("TAK", "NIE"),
+      animation = "tada",
+      status = "default"
+    ),
+    
+    actionButton("button", "kliknij tutaj, by wylosować"),
+    
     ),
   
   
   mainPanel(
     textOutput("tekst"),
-    verbatimTextOutput("value")
-            )
+    
+    verbatimTextOutput("value"),
+    
+    plotOutput("plot", click = "plot_click"),
+           )
 )
 
 server = function(input, output, session) {
   
   observeEvent(input$button, {
     
+    if(input$radio == "TAK"){ 
+    output$plot = renderPlot({
+      ggplot(wzrost_cen, aes(x=procent,
+                             y=rok)) + stat_smooth()  + geom_point() +ggtitle("Procentowy wzrost średniej ceny, na podstawie dania głownego")+theme(plot.title=element_text(hjust=0.5))
+    }, res = 96)}
+   
+    
    output$tekst = renderText({ paste("Twój wylosowany zestaw to: ")})
     
    output$value =  renderPrint({ 
      if(input$wege == TRUE){
-       zestaw_wege("O", "DS", "N")
+       zestaw_wege(input$kwota)
        }
      else{
-       zestaw("O", "DS", "N")
+       zestaw(input$kwota)
      }
   })
+   
+  
     
   }
 )}
 
 shinyApp(ui, server)
+
